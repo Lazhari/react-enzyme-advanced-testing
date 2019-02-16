0636f5010c3846c522c9f000bbefdbdc33b52fcf
Mohammed Lazhari <lazhari.mohammed@outlook.com>
2019-02-16 18:09:03 +0100

    Update the comments reducer

--- src/reducers/comments.js
+++ src/reducers/comments.js
@@ -1,7 +1,10 @@
+import { SAVE_COMMENT } from 'actions/types';
 const initialState = [];
 
 export default function(state = initialState, action) {
 	switch (action.type) {
+		case SAVE_COMMENT:
+			return [...state, action.payload];
 		default:
 			return state;
 	}
