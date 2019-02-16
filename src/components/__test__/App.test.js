54de1ad68f8c71ab2afa0f563d006c7b9e4f7118
Mohammed Lazhari <lazhari.mohammed@outlook.com>
2019-02-16 17:44:05 +0100

    Refactoring the test of the App and CommentBox

--- src/components/__test__/App.test.js
+++ src/components/__test__/App.test.js
@@ -4,17 +4,15 @@ import App from 'components/App';
 import CommentBox from 'components/CommentBox';
 import CommentList from 'components/CommentList';
 
-describe('Testing App component', () => {
-	let wrapper;
-	beforeEach(() => {
-		// Because we are using withStyles HoC component from material-ui
-		wrapper = shallow(<App />).dive();
-	});
-	it('shows a comment box', () => {
-		expect(wrapper.find(CommentBox).length).toEqual(1);
-	});
+let wrapper;
+beforeEach(() => {
+	// Because we are using withStyles HoC component from material-ui
+	wrapper = shallow(<App />).dive();
+});
+it('shows a comment box', () => {
+	expect(wrapper.find(CommentBox).length).toEqual(1);
+});
 
-	it('shows a comment list', () => {
-		expect(wrapper.find(CommentList).length).toEqual(1);
-	});
+it('shows a comment list', () => {
+	expect(wrapper.find(CommentList).length).toEqual(1);
 });
--- src/components/__test__/CommentBox.test.js
+++ src/components/__test__/CommentBox.test.js
@@ -2,39 +2,37 @@ import React from 'react';
 import { mount } from 'enzyme';
 import CommentBox from 'components/CommentBox';
 
-describe('Testing the CommentBox', () => {
-	let wrapper;
+let wrapper;
 
+beforeEach(() => {
+	wrapper = mount(<CommentBox />);
+});
+
+it('has a h1 title, form, textarea and a button ', () => {
+	expect(wrapper.find('h1').length).toEqual(1);
+	expect(wrapper.find('form').length).toEqual(1);
+	expect(wrapper.find('textarea').length).toEqual(1);
+	expect(wrapper.find('button').length).toEqual(1);
+});
+
+describe('Testing the comment form', () => {
 	beforeEach(() => {
-		wrapper = mount(<CommentBox />);
-	});
-
-	it('has a h1 title, form, textarea and a button ', () => {
-		expect(wrapper.find('h1').length).toEqual(1);
-		expect(wrapper.find('form').length).toEqual(1);
-		expect(wrapper.find('textarea').length).toEqual(1);
-		expect(wrapper.find('button').length).toEqual(1);
-	});
-
-	it('has a text area users can type in', () => {
 		const textarea = wrapper.find('textarea');
 		textarea.simulate('change', {
 			target: { value: 'New comment' }
 		});
+	});
+	it('has a textarea, users can edit it', () => {
 		expect(wrapper.find('textarea').prop('value')).toEqual('New comment');
 	});
 
-	it('has a textarea empty after user submit the form', () => {
+	it('has a textarea empty after, the user submit the form', () => {
 		const form = wrapper.find('form');
-		const textarea = wrapper.find('textarea');
-		textarea.simulate('change', {
-			target: { value: 'New comment' }
-		});
 		form.simulate('submit');
 		expect(wrapper.find('textarea').prop('value')).toEqual('');
 	});
+});
 
-	afterEach(() => {
-		wrapper.unmount();
-	});
+afterEach(() => {
+	wrapper.unmount();
 });
