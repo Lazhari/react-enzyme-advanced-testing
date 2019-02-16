9c5e13a89de6f00be008282871916aaa3b82e151
Mohammed Lazhari <lazhari.mohammed@outlook.com>
2019-02-16 21:51:25 +0100

    Update and test the comment list component

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,7 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { compose } from 'recompose';
+import { connect } from 'react-redux';
 
-const CommentList = () => {
-	return <div>Comment List Component</div>;
-};
+import { withStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+class CommentList extends Component {
+	renderComments() {
+		const { comments } = this.props;
+		return comments.map((comment, index) => (
+			<ListItem key={index}>
+				<ListItemText>{comment}</ListItemText>
+			</ListItem>
+		));
+	}
+	render() {
+		const { classes } = this.props;
+		return <List className={classes.root}>{this.renderComments()}</List>;
+	}
+}
 
-export default CommentList;
+const styles = ({ spacing, palette }) => ({
+	root: {
+		marginTop: spacing.unit * 2,
+		width: '100%',
+		backgroundColor: palette.background.paper
+	}
+});
+
+function mapStateToProps(state) {
+	const { comments } = state;
+	return { comments };
+}
+
+export default compose(
+	connect(mapStateToProps),
+	withStyles(styles)
+)(CommentList);
