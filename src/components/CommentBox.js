d6b638f9923421bfacf6e836c29e9513d74f8b51
Mohammed Lazhari <lazhari.mohammed@outlook.com>
2019-02-16 16:06:39 +0100

    Create the comment box component

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,7 +1,69 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { createStyles, withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
-const CommentBox = () => {
-	return <div>Comment Box Component</div>;
-};
+class CommentBox extends Component {
+	state = { comment: '' };
+	handleChange = event => {
+		this.setState({
+			comment: event.target.value
+		});
+	};
+	handleSubmit = event => {
+		event.preventDefault();
+		// TODO: Call an action create
+		// And Save the comment entered by the user
 
-export default CommentBox;
+		this.setState({ comment: '' });
+	};
+	render() {
+		const { classes } = this.props;
+
+		return (
+			<Card>
+				<CardContent>
+					<Typography variant="title" className={classes.title}>
+						Add Comment
+					</Typography>
+					<form onSubmit={this.handleSubmit}>
+						<TextField
+							value={this.state.comment}
+							onChange={this.handleChange}
+							variant="outlined"
+							multiline
+							fullWidth
+							rows={4}
+							label="Your Comment"
+						/>
+						<Button
+							type="submit"
+							color="primary"
+							variant="contained"
+							className={classes.button}
+							fullWidth
+						>
+							Submit Comment
+						</Button>
+					</form>
+				</CardContent>
+			</Card>
+		);
+	}
+}
+
+const styles = ({ spacing }) =>
+	createStyles({
+		root: {},
+		title: {
+			marginBottom: spacing.unit * 2
+		},
+		button: {
+			marginTop: spacing.unit * 2
+		}
+	});
+
+export default withStyles(styles)(CommentBox);
