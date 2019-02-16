453c3a1933edbd3024c948e3f888d210f5862739
Mohammed Lazhari <lazhari.mohammed@outlook.com>
2019-02-16 17:13:48 +0100

    Testing the form submit and the textare change

--- src/components/__test__/CommentBox.test.js
+++ src/components/__test__/CommentBox.test.js
@@ -8,15 +8,30 @@ describe('Testing the CommentBox', () => {
 	beforeEach(() => {
 		wrapper = mount(<CommentBox />);
 	});
-	it('has a h1 title', () => {
+
+	it('has a h1 title, form, textarea and a button ', () => {
 		expect(wrapper.find('h1').length).toEqual(1);
-	});
-	it('has a textarea field', () => {
+		expect(wrapper.find('form').length).toEqual(1);
 		expect(wrapper.find('textarea').length).toEqual(1);
+		expect(wrapper.find('button').length).toEqual(1);
 	});
 
-	it('has a button', () => {
-		expect(wrapper.find('button').length).toEqual(1);
+	it('has a text area users can type in', () => {
+		const textarea = wrapper.find('textarea');
+		textarea.simulate('change', {
+			target: { value: 'New comment' }
+		});
+		expect(wrapper.find('textarea').prop('value')).toEqual('New comment');
+	});
+
+	it('has a textarea empty after user submit the form', () => {
+		const form = wrapper.find('form');
+		const textarea = wrapper.find('textarea');
+		textarea.simulate('change', {
+			target: { value: 'New comment' }
+		});
+		form.simulate('submit');
+		expect(wrapper.find('textarea').prop('value')).toEqual('');
 	});
 
 	afterEach(() => {
