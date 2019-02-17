eb5796f1ff942f2df8b73a0e135c2f82f9546aa6
Mohammed Lazhari <lazhari.mohammed@outlook.com>
2019-02-17 10:27:22 +0100

    Add reach router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router } from '@reach/router';
 
 import Root from 'Root';
 import App from 'components/App';
 
 ReactDOM.render(
 	<Root>
-		<App />
+		<Router>
+			<App path="/" />
+		</Router>
 	</Root>,
 	document.getElementById('root')
 );
