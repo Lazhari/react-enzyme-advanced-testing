cb2c77d2c386f542cd8ca82ffdcc747a8a15cb1b
Mohammed Lazhari <lazhari.mohammed@outlook.com>
2019-02-17 10:55:33 +0100

    Testing the routes

--- src/components/__test__/App.test.js
+++ src/components/__test__/App.test.js
@@ -1,18 +1,55 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
+import { MemoryRouter } from 'react-router-dom';
 import App from 'components/App';
+import Root from 'Root';
 import CommentBox from 'components/CommentBox';
 import CommentList from 'components/CommentList';
 
-let wrapper;
-beforeEach(() => {
-	// Because we are using withStyles HoC component from material-ui
-	wrapper = shallow(<App />).dive();
-});
-it('shows a comment box', () => {
-	expect(wrapper.find(CommentBox).length).toEqual(1);
+describe('Testing the post route', () => {
+	let wrapper;
+	beforeEach(() => {
+		// Because we are using withStyles HoC component from material-ui
+		wrapper = mount(
+			<MemoryRouter initialEntries={['/post']}>
+				<Root>
+					<App />
+				</Root>
+			</MemoryRouter>
+		);
+	});
+	it('shows a comment box', () => {
+		expect(wrapper.find(CommentBox).length).toEqual(1);
+	});
+
+	it('shows a comment list', () => {
+		expect(wrapper.find(CommentList).length).toEqual(1);
+	});
+	afterEach(() => {
+		wrapper.unmount();
+	});
 });
 
-it('shows a comment list', () => {
-	expect(wrapper.find(CommentList).length).toEqual(1);
+describe('Testing the / route', () => {
+	let wrapper;
+	beforeEach(() => {
+		// Because we are using withStyles HoC component from material-ui
+		wrapper = mount(
+			<MemoryRouter initialEntries={['/']}>
+				<Root>
+					<App />
+				</Root>
+			</MemoryRouter>
+		);
+	});
+	it('shows no comment box', () => {
+		expect(wrapper.find(CommentBox).length).toEqual(0);
+	});
+
+	it('shows a comment list', () => {
+		expect(wrapper.find(CommentList).length).toEqual(1);
+	});
+	afterEach(() => {
+		wrapper.unmount();
+	});
 });
